5dfad49af77bf02dd5eea2e3ec556b4bdb7001de
lesmadeit <lesmwendwa@gmail.com>
2025-01-06 10:35:21 +0300

    tests

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+from . import apiviews
+
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = apiviews.PollViewSet.as_view({'get':'list'})
+        self.uri = '/polls/'
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
