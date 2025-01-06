4ffd60d4b2bebbe9f6dac77527db62c422181bcc
lesmadeit <lesmwendwa@gmail.com>
2025-01-06 11:05:32 +0300

    changes

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIRequestFactory
-
 from . import apiviews
 
 
@@ -8,9 +9,23 @@ class TestPoll(APITestCase):
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get':'list'})
         self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@gmail.com',
+            password='test'
+        )
 
     def test_list(self):
-        request = self.factory.get(self.uri)
+        request = self.factory.get(self.uri,
+                                   HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
         response = self.view(request)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
